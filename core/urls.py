01efd947c7c92f815ddb160ef183238fe9aa2b62
Jarbas Peixoto Júnior <jarbas.junior@gmail.com>
2025-01-22 19:19:13 -0400

    Change main app name

--- core/urls.py
+++ core/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', include('directory.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
