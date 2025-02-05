9c1537ca7bfeb479d1da3732214856c1af995ce0
Jarbas Peixoto Junior <jarbas.junior@gmail.com>
2025-02-05 08:03:42 -0400

    Add models into administrator area

--- directory/admin.py
+++ directory/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ADUser, ADGroup, AuditLog, ADOperation
+
+admin.site.register(ADUser)
+admin.site.register(ADGroup)
+admin.site.register(AuditLog)
+admin.site.register(ADOperation)
+
--- directory/models.py
+++ directory/models.py
@@ -1,7 +1,63 @@
 from django.db import models
-
-# Create your models here.
 from django.conf import settings
+
+class ADUser(models.Model):
+    """Model to represent Active Directory users"""
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    department = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    enabled = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'AD User'
+        verbose_name_plural = 'AD Users'
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+class ADGroup(models.Model):
+    """Model to represent Active Directory groups"""
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    members = models.ManyToManyField(ADUser, related_name='groups', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'AD Group'
+        verbose_name_plural = 'AD Groups'
+
+    def __str__(self):
+        return self.name
+
+class AuditLog(models.Model):
+    """Model to track changes made to AD objects"""
+    ACTION_CHOICES = [
+        ('CREATE', 'Create'),
+        ('MODIFY', 'Modify'),
+        ('DELETE', 'Delete'),
+        ('ENABLE', 'Enable'),
+        ('DISABLE', 'Disable'),
+        ('PASSWORD', 'Password Reset'),
+    ]
+
+    action = models.CharField(max_length=20, choices=ACTION_CHOICES)
+    actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    target_type = models.CharField(max_length=50)  # 'user' or 'group'
+    target_name = models.CharField(max_length=100)
+    details = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"{self.action} {self.target_type}: {self.target_name} by {self.actor}"
 
 class ADOperation(models.Model):
     """Log of AD operations performed by users"""
