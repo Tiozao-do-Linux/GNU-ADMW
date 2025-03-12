037da1c1bb01af3e44b8467b494a077b3de1a025
Jarbas Peixoto Júnior <jarbas.junior@gmail.com>
2025-03-12 19:48:34 -0400

    DRY (Don’t Repeat Yourself) in test.py

--- directory/tests.py
+++ directory/tests.py
@@ -37,22 +37,19 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 
 class HomepageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        # DRY (Don’t Repeat Yourself)
+        url = reverse("home")
+        self.response = self.client.get(url)
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template_name(self):
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(self.response, "home.html")
 
     def test_homepage_contains_correct_html(self):
-        response = self.client.get("/")
-        self.assertContains(response, "Home Page")
+        self.assertContains(self.response, "Home Page")
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        response = self.client.get("/")
-        self.assertNotContains(response, "Hi there! I should not be on the page.")
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
