e37dec5e820e27c1755b04abfc92143e3dbedf8f
Jarbas Peixoto Júnior <jarbas.junior@gmail.com>
2025-04-06 18:51:16 -0400

    Display IPv4 and IPv6 like remote_ip

--- core/middleware.py
+++ core/middleware.py
@@ -1,5 +1,6 @@
 from user_agents import parse
 import time
+import ipaddress
 
 class RenderTimeAndBrowserMiddleware:
     def __init__(self, get_response):
@@ -13,12 +14,34 @@ class RenderTimeAndBrowserMiddleware:
         user_agent = parse(user_agent_str)
         request._browser_info = f"{user_agent.browser.family} / {user_agent.browser.version_string} / {user_agent.os.family} /"
 
-        # Remote IP
+        # Collect IPs and separate by type
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+        ip_list = []
+
         if x_forwarded_for:
-            request._remote_ip = x_forwarded_for.split(',')[0].strip()
+            ip_list = [ip.strip() for ip in x_forwarded_for.split(',')]
         else:
-            request._remote_ip = request.META.get('REMOTE_ADDR', 'Unknow')
+            remote_ip = request.META.get('REMOTE_ADDR', '')
+            if remote_ip:
+                ip_list = [remote_ip]
+
+        ipv4 = []
+        ipv6 = []
+
+        for ip in ip_list:
+            try:
+                ip_obj = ipaddress.ip_address(ip)
+                if ip_obj.version == 4:
+                    ipv4.append(ip)
+                elif ip_obj.version == 6:
+                    ipv6.append(ip)
+            except ValueError:
+                continue  # Ignora IPs inválidos
+
+        ipv4_str = ', '.join(ipv4) if ipv4 else ''
+        ipv6_str = ', '.join(ipv6) if ipv6 else ''
+        request._remote_ip = f"{ipv4_str}{ipv6_str}"
 
         return self.get_response(request)
 
